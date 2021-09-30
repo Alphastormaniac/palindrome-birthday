887fcfb86ee5dac68725cf3142ad18c0e804ceff
Sai Pavan <sai.pavan7829@gmail.com>
2021-09-30 20:59:04 +0530

    updated: js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function reverseString(string){
-    var listOfChars = string.split(''); 
+function reverseString(string) {
+    var listOfChars = string.split('');
     var reverseListOfChars = listOfChars.reverse();
     var reversedString = reverseListOfChars.join('');
     return reversedString;
@@ -7,22 +7,26 @@ function reverseString(string){
 }
 
 
-function isPalindrome(string){
+function isPalindrome(string) {
     const reverse = reverseString(string);
-    return(string === reverse)  
+    return (string === reverse)
 }
 
 
-function convertDateToString(date){
-    const dateString = {day: '', month: '', year: ''};
+function convertDateToString(date) {
+    const dateString = {
+        day: '',
+        month: '',
+        year: ''
+    };
 
-    if(date.day < 10){
-        dateString.day = "0" + date.day; 
+    if (date.day < 10) {
+        dateString.day = "0" + date.day;
     } else {
         dateString.day = date.day.toString();
     }
-    if(date.month < 10){
-        dateString.month = "0" + date.month; 
+    if (date.month < 10) {
+        dateString.month = "0" + date.month;
     } else {
         dateString.month = date.month.toString();
     }
@@ -31,13 +35,7 @@ function convertDateToString(date){
     return dateString
 }
 
-var date = {
-    day: 12,
-    month: 11,
-    year: 2020
-};
-
-function getAllDateFormats(date){
+function getAllDateFormats(date) {
     const dateString = convertDateToString(date)
 
     const ddmmyyyy = dateString.day + dateString.month + dateString.year;
@@ -49,18 +47,76 @@ function getAllDateFormats(date){
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
 }
 
-function checkPalindromeForAllDateFormats(date){
+function checkPalindromeForAllDateFormats(date) {
     const listOfPalindromes = getAllDateFormats(date)
 
     var flag = false;
 
-    for(var i=0; i< listOfPalindromes.length; i++){
-        if (isPalindrome(listOfPalindromes[i])){
+    for (var i = 0; i < listOfPalindromes.length; i++) {
+        if (isPalindrome(listOfPalindromes[i])) {
             flag = true;
             break;
         }
     }
     return flag;
 }
+
+function leapYear(year) {
+    if (year % 400 === 0) {
+        return true
+    }
+    if (year % 100 === 0) {
+        return false
+    }
+    if (year % 4 === 0) {
+        return true;
+    }
+    return false;
+}
+
+function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (month === 2){
+        // check for leap year
+        if(leapYear(year)){
+            if(day>29){
+                day = 1;
+                month++;
+            }
+        }
+        else {
+            if(day >28){
+                day = 1;
+                month++;
+            }
+        }
+    } 
+    else {
+        if(day > daysInMonth[month - 1]){
+            day= 1;
+            month++;
+        }
+    }
+
+    if(month > 12){
+        month = 1;
+        year++ 
+    }
+}
+
+function getNextPalindromeDate(date) {
+
+}
+
+var date = {
+    day: 12,
+    month: 11,
+    year: 2020
+};
 
 console.log(checkPalindromeForAllDateFormats(date))
